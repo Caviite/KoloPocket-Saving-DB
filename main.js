b2e74420e0dff1d4fa701bff7e3e7c520a2ff20b
caviite <yomidave8682@gmail.com>
2026-05-22 00:40:52 +0100

    vercel

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const authpageRoute = require('./src/routes/authpage');
 
-app.use(cors({origin: 'http://localhost:5173' }));
+app.use(cors({origin: 'http://kolopocket.vercel.app' }));
 app.use(express.json());
 app.use("/auth", authpageRoute);
 
